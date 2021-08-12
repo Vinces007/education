d52bf1c30afd14b5892c4be206ccb09d7d3e3ef3
vince007 <13840148193@163.com>
2021-08-12 16:38:13 +0800

    eureka

--- edu-student-server/src/main/java/com/edu/stu/edustudentserverMain.java
+++ edu-student-server/src/main/java/com/edu/stu/edustudentserverMain.java
@@ -2,8 +2,10 @@ package com.edu.stu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class edustudentserverMain {
     public static void main(String[] args) {
         SpringApplication.run(edustudentserverMain.class,args);
